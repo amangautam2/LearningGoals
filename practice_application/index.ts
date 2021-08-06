48e273c2ca12d1ddf63f1260595cf04798cfcea8
aman <gautamaman30@gmail.com>
2021-08-06 13:10:39 +0530

    Adding stream

--- practice_application/index.ts
+++ practice_application/index.ts
@@ -2,10 +2,41 @@ import { createServer } from "http";
 import express from "express";
 import Config from "./config";
 import asyncHook from "./trackAsyncResource";
+import { createWriteStream, createReadStream } from "fs";
 
 const app: express.Application = express();
-const server = createServer(app);
 
+
+app.get('', (req, res) => {
+    const { text } = req.query;
+    if(!text) {
+        res.send("No text in query found");
+    }
+
+    const writer = createWriteStream("text.txt");
+    writer.write(text);
+    writer.on("error", error => {
+        console.error(error);
+    });
+
+
+    //pipe read stream to response stream
+    let data = '';
+    const reader = createReadStream("text.txt");
+    //reader.pipe(res);
+    reader.on("data", chunk => {
+        data += chunk;
+    })
+    reader.on("end", () => {
+        res.send(data);
+    });
+    reader.on("error", error => {
+        console.error(error);
+    });
+})
+
+
+const server = createServer(app);
 asyncHook.enable(); 
 
 server.listen(
